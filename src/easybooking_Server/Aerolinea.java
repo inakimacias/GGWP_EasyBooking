81c764cc48da32b0c835df6df45f3fa1189391be
Daniel Diez-Andino Lopez <diezandino.daniel@opendeusto.es>
2019-11-06 18:38:38 +0100

    Cambios

--- src/easybooking_Server/Aerolinea.java
+++ src/easybooking_Server/Aerolinea.java
@@ -1,5 +1,16 @@
 package easybooking_Server;
 
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
 public class Aerolinea {
+	
+	String idAerolinea = null;
+	String nombreAerolinea = null;
+	
+	public Aerolinea(String idAerolinea, String nombreAerolinea) {
+		idAerolinea = this.idAerolinea;
+		nombreAerolinea = this.nombreAerolinea;
+	}
 
 }
--- src/easybooking_Server/Reserva.java
+++ src/easybooking_Server/Reserva.java
@@ -1,5 +1,23 @@
 package easybooking_Server;
 
-public class Reserva {
+import java.util.Date;
 
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class Reserva {	
+
+	@PrimaryKey
+	String idReserva = null;
+	int numPasajeros = 0;
+	Date fecha = null;
+	double hora = 0;
+
+	public Reserva(String idReserva, int numPasajeros, Date fecha, double hora) {
+		this.idReserva = idReserva;
+		this.numPasajeros = numPasajeros;
+		this.fecha = fecha;
+		this.hora = hora;
+	}
 }
