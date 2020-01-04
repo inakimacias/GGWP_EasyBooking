23b4a003c5d51b68c7b565512059902771da46e2
Daniel Diez-Andino Lopez <diezandino.daniel@opendeusto.es>
2020-01-04 19:49:46 +0100

    Codigo Inicial añadido a EasyBookingManager

--- src/remote/EasyBookingManager.java
+++ src/remote/EasyBookingManager.java
@@ -1,5 +1,44 @@
 package remote;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import dto.DTOUsuario;
+import dto.DTOVuelo;
+import jdo.Usuario;
+
 public class EasyBookingManager {
 
+	private static EasyBookingManager instance;
+	public Usuario state;
+	
+	private EasyBookingManager() throws RemoteException {
+		super();		
+	}
+	
+	public static EasyBookingManager getInstance() {
+		if (instance == null) {
+			try {
+				instance = new EasyBookingManager();
+			} catch (Exception ex) {
+				System.err.println("# Error creating RemoteFacade: " + ex);
+			}
+		}
+		return instance;
+	}
+	
+	public boolean login(String email, String password) {		
+		// this.state = LoginService.getInstance().login(email, password);
+		
+		return state != null;
+	}
+	
+	public void buscarVuelo(String nombreOrigen, String nombreDestino) {
+		
+	}
+	
+	public void reservar(DTOUsuario usuario, DTOVuelo vuelo, ArrayList<String> nombres, String idCuenta, int coste) {
+		
+	}
+	
 }
--- src/remote/IEasyBookingManager.java
+++ src/remote/IEasyBookingManager.java
@@ -1,5 +1,16 @@
 package remote;
 
-public class IEasyBookingManager {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import dto.DTOUsuario;
+import dto.DTOVuelo;
+
+public interface IEasyBookingManager extends Remote {
+
+	public boolean login(String email, String password) throws RemoteException;
+	public void reservar(DTOUsuario usuario, DTOVuelo vuelo, ArrayList<String> nombres, String idCuenta, int coste) throws RemoteException;
+	public void buscarVuelo(String nombreOrigen, String nombreDestino) throws RemoteException;
 
 }
