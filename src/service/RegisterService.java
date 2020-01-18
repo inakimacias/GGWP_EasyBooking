e8eb86e5920bc973950254efc31e842be3ec9d46
inakimacias <inakimacias@opendeusto.es>
2020-01-18 19:19:33 +0100

    z

--- src/service/RegisterService.java
+++ src/service/RegisterService.java
@@ -1,16 +1,5 @@
 package service;
 
-import java.util.ArrayList;
-import java.util.Date;
-
-import dao.DBManager;
-import gateways.IAerolineaGw;
-import gateways.IPagoGw;
-import jdo.InformePago;
-import jdo.Reserva;
-import jdo.Usuario;
-import jdo.Vuelo;
-
 public class RegisterService {
 private static RegisterService instance;
 	
@@ -24,33 +13,8 @@ private static RegisterService instance;
 		return instance;
 	}
 	
-	public void registro(Usuario usuario, Vuelo vuelo, String nombres, String authType, String idCuenta) {
-		ArrayList<String> goodData = new ArrayList<String>();
-		Date d = new Date();
-		for(int i = 0; i<nombres.split(";").length; i++) {
-			goodData.add(nombres.split(";")[i]);
-		}
-		IAerolineaGw agw = Factory.getInstance().createAerolineaGw(vuelo.getAerolinea());
-		IPagoGw pgw = Factory.getInstance().createPagoGw(authType);
+	public void registro(String id, String password, String authType) {
 		
-		agw.reservarVuelo(vuelo.getIdVuelo(), goodData.size());
-		
-		pgw.pagar(idCuenta, vuelo.getPrecio()*goodData.size());
-		
-		InformePago ip = new InformePago();
-		ip.setCoste(vuelo.getPrecio()*goodData.size());
-		ip.setFecha(d.toString());
-		ip.setMetodoPago(authType);
-		ip.setIdCuenta(idCuenta);
-		DBManager.getInstance().guardarInforme(ip);
-		
-		Reserva r = new Reserva();
-		r.setNumPasajeros(goodData.size());
-		r.setNombrePasajeros(nombres);
-		r.setFecha(d.toString());
-		r.setUsuario(usuario);
-		r.setVuelo(vuelo);
-		DBManager.getInstance().guardarReserva(r);
 	}
 
 }
