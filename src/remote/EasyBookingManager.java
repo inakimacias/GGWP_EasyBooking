a857861d22d4ce48f4351a6b16f4ff6d3e43af69
inakimacias <inakimacias@opendeusto.es>
2020-01-18 20:44:17 +0100

    z

--- src/remote/EasyBookingManager.java
+++ src/remote/EasyBookingManager.java
@@ -1,5 +1,6 @@
 package remote;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
@@ -14,21 +15,31 @@ import service.ReservaService;
 public class EasyBookingManager extends UnicastRemoteObject implements IEasyBookingManager {
 
 	private static final long serialVersionUID = 1L;
-	private static EasyBookingManager instance;
 	
 	private EasyBookingManager() throws RemoteException {
 		super();		
 	}
 	
-	public static EasyBookingManager getInstance() {
-		if (instance == null) {
-			try {
-				instance = new EasyBookingManager();
-			} catch (Exception ex) {
-				System.err.println("# Error creating RemoteFacade: " + ex);
-			}
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.out.println("usage: java [policy] [codebase] server.Server [host] [port] [server]");
+			System.exit(0);
 		}
-		return instance;
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
+
+		try {		
+			IEasyBookingManager objServer = new EasyBookingManager();
+			Naming.rebind(name, objServer);
+			System.out.println("* Server '" + name + "' active and waiting...");
+		} catch (Exception e) {
+			System.err.println("- Exception running the server: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean registrarse(String id, String password, String authType) throws RemoteException {
