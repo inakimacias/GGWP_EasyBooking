85361ff213801a9e9e80e80daff97d6fb851d56b
inakimacias <inakimacias@opendeusto.es>
2020-01-18 18:57:46 +0100

    z

--- src/service/BuscarVueloService.java
+++ src/service/BuscarVueloService.java
@@ -2,6 +2,8 @@ package service;
 
 import java.util.ArrayList;
 
+import assemblers.Assembler;
+import dto.DTOVuelo;
 import gateways.IberiaGw;
 import gateways.RyannairGw;
 import gateways.VuelingGw;
@@ -21,12 +23,16 @@ public class BuscarVueloService {
 		return instance;
 	}
 	
-	public ArrayList<Vuelo> buscarVuelo(String origen, String destino) {
+	public ArrayList<DTOVuelo> buscarVuelo(String origen, String destino) {
 		ArrayList<Vuelo> vuelos = new ArrayList<Vuelo>();
+		ArrayList<DTOVuelo> DTOvuelos = new ArrayList<DTOVuelo>();
 		vuelos.addAll(IberiaGw.getInstance().buscarVuelo(origen, destino));
 		vuelos.addAll(RyannairGw.getInstance().buscarVuelo(origen, destino));
 		vuelos.addAll(VuelingGw.getInstance().buscarVuelo(origen, destino));
-		return vuelos;
+		for(int i = 0; i < vuelos.size(); i++) {
+			DTOvuelos.add(Assembler.getInstance().assemble(vuelos.get(i)));
+		}
+		return DTOvuelos;
 	}
 
 }
